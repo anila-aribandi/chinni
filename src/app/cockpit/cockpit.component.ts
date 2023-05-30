1c0a8899f722262a7a5d51991f742d3aa561af02
anila-aribandi <anilachoudary99@gmail.com>
2023-05-30 18:00:47 +0530

    first commit

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -10,18 +10,19 @@ export class CockpitComponent implements OnInit {
   //Add paranthesis in the end to call the constructor of eventemitter and create
   // a new eventemitter object which is stored in servercreated
  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  newServerName = '';
+  //newServerName = '';
   newServerContent = '';
 
   constructor() { }
 
   ngOnInit(): void {
   }
-  onAddServer(){
-    this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
+  onAddServer(nameInput: HTMLInputElement){
+    console.log(nameInput.value);
+    this.serverCreated.emit({serverName: nameInput.value, serverContent: this.newServerContent});
   }
-  onAddBlueprint(){
-    this.blueprintCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
+  onAddBlueprint(nameInput: HTMLInputElement){
+    this.blueprintCreated.emit({serverName: nameInput.value, serverContent: this.newServerContent});
   }
 
 }
